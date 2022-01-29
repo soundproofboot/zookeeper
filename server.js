755863ba1a0f32d6b57404bb8d6a884b59329fd5
C Bares <colinedwinbares@gmail.com>
2022-01-29 16:16:04 -0600

    added front end files and paths in server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const path = require("path");
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static("public"));
 
 // helper functions
 function filterByQuery(query, animalsArray) {
@@ -119,6 +120,22 @@ app.post("/api/animals", (req, res) => {
   }
 
   res.json(animal);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/animals", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/animals.html"));
+});
+
+app.get("/zookeepers", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
